8d29ce076063d23cae3dc1d22976418cb400f2f7
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-18 14:38:31 +0300

    Add TextField component

---
+++ src/components/TextField.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="text-field">
+    <label for="text-field">
+      <slot>{{ title }}</slot>
+    </label>
+    <input type="text" id="text-field" />
+  </div>
+</template>
