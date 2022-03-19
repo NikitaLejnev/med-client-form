5597bf791e01c58aa323c44c56656cfac56a547f
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-19 08:37:21 +0300

    Add form fields

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,11 +1,20 @@
 <script setup>
+import { computed, reactive } from 'vue'
+import useVuelidate from '@vuelidate/core'
+
+const props = defineProps({
+  number: Number,
+  required: Boolean
+})
+
+const inputId = computed(() => (`text-field-${props.number}`))
 </script>
 
 <template>
   <div class="text-field">
-    <label for="text-field">
+    <label :for="`${inputId}`">
       <slot>{{ title }}</slot>
     </label>
-    <input type="text" id="text-field" />
+    <input type="text" :id="`${inputId}`" />
   </div>
 </template>
