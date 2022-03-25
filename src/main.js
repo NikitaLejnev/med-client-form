93521697d951bf0f1b7fe1b56c32e73653509ae4
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-25 17:23:10 +0300

    Configure for deployment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+Vue.config.devtools = true;
+
+createApp(App).mount("#app");
