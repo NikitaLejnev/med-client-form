9dd3e6626bc4c509c7b69c9b9e9362aa73dcb92b
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-25 18:27:58 +0300

    Configure for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/med-client-form/",
+  define: {
+    __VUE_PROD_DEVTOOLS__: true,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
